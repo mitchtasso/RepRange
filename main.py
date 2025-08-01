e74289303aeab98f5f2da40637b0d9001388d9d9
mitchtasso <145378736+mitchtasso@users.noreply.github.com>
2025-07-31 20:23:16 -0400

    Added pass through for user data to AI and ability to clear chat

--- main.py
+++ main.py
@@ -113,22 +113,28 @@ def main_page(user_email, user_id, token):
     
     elif page == "🦾ChadAI":
         logo, title = st.columns([1,5])
+        
+        def reset_chat():
+            st.session_state['messages'] = []
+            response = supabase.table("workouts").select("workout_date, exercise_type, repetitions, weight, sets").eq("user_id", user_id).order("workout_date", desc=True).execute()
+            st.session_state['messages'].append({"role": "user", "content": f'Act like a fitness guru named ChadAI and use these workout records for reference: {str(response.data)}'})
 
         with logo:
             st.image("chadAI.png", width=100)
         with title:
             st.title("ChadAI")
-            github, linkedin, streamlit, space = st.columns([1,1,1,1.5])
-
+            clear = st.button("Clear")
+            if clear:
+                reset_chat()
+        
         if 'model' not in st.session_state:
             st.session_state['model'] = OpenAI(api_key=openai_key)
 
         if 'messages' not in st.session_state:
-            st.session_state['messages'] = []
-            st.session_state['messages'].append({"role": "user", "content": 'Act like a fitness guru named ChadAI'})
+            reset_chat()
 
         for message in st.session_state['messages']:
-            if message['content'] != 'Act like a fitness guru named ChadAI':
+            if 'Act like a fitness guru named ChadAI' not in message['content']:
                 with st.chat_message(message['role']):
                     st.markdown(message['content'])
 
@@ -145,7 +151,7 @@ def main_page(user_email, user_id, token):
                         {"role": message["role"], "content": message["content"]} for message in st.session_state['messages']
                     ],
                     temperature=0.7,
-                    max_tokens=256,
+                    max_tokens=512,
                     stream=True
                 )
 
